eab21a5cce37c1ab9930a27b592d98c5c3b06270
Code-N-Code <icodencode@gmail.com>
2025-01-25 15:29:02 +0530

    Updating Main class.

--- src/main/java/com/codencode/Main.java
+++ src/main/java/com/codencode/Main.java
@@ -2,6 +2,6 @@ package com.codencode;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("5 + 7 = " + new MathLibrary().add(5, 7));
     }
 }
