987cf739837a5c06cce8f6d5f727d3f780daf396
Code-N-Code <icodencode@gmail.com>
2025-01-25 14:57:28 +0530

    Initial commit.

---
+++ src/main/java/com/codencode/Main.java
@@ -0,0 +1,7 @@
+package com.codencode;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/codencode/MathLibrary.java
@@ -0,0 +1,19 @@
+package com.codencode;
+
+public class MathLibrary {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int product(int a, int b) {
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        return a/b;
+    }
+}
